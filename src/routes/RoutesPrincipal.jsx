01eddabf6a43d19cf7d9456899b1ab177463ca8f
Celeste Mariel Soto <celestemariel28@gmail.com>
2025-12-15 15:41:00 -0300

    Creo el comopnente routesPrincipal para el manejo de rutas

--- src/routes/RoutesPrincipal.jsx
+++ src/routes/RoutesPrincipal.jsx
@@ -4,6 +4,20 @@ import LoginScreen from "../views/LoginScreen";
 const RoutesPrincipal = () => {
   return (
     <Routes>
+      {/* <Route
+        path="/"
+        element={
+          <ProtectedRoute>
+            <PagesLayout />
+          </ProtectedRoute>
+        }
+      >
+        <Route index element={<HomeScreen />} />
+        <Route path="productos/:id" element={<ProductosScreen/>}/>
+        <Route path="sobrenosotros" element={<SobreNosotros/>}/>
+        <Route path="*" element={<Error404Screen />} />
+
+      </Route> */}
         <Route path="login" element={<LoginScreen/>}/>
     </Routes>
   )
