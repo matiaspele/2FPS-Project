0dea907b24243a9f2fba6248cdc94a9ad0e79ff4
Matias Pelegrina <mati.pelegrina@hotmail.com>
2025-12-18 19:03:36 -0300

    cambios finales para merge

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,7 @@ import Error404 from "./components/Error404"
 import Header from "./components/Header"
 
 const App = () => {
-  return (
-    <div>
-      <Header/>
-    </div>
-  )
+ 
 }
 
 export default App
